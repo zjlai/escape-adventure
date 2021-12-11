adbb7b116918efd675f0f8c7b800566e9c44e727
ZJ <zijian.lai@protonmail.com>
2021-12-11 12:45:51 +0800

    Remove test Code

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -8,11 +8,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue'
+import { defineComponent, computed } from 'vue'
 import GameTitle from 'src/components/game/title.vue'
 import StoryCarousel from 'src/components/game/storyCarousel.vue'
 import { useRoute } from 'vue-router'
-import { createGame } from 'src/apis/firebaseApis'
+// import { createGame } from 'src/apis/firebaseApis'
 // import { apiService } from 'src/apis/prismicApis'
 
 export default defineComponent({
@@ -23,11 +23,12 @@ export default defineComponent({
   },
   setup() {
     const route = useRoute()
-    onMounted(async () => {
-      console.log('data')
-      const data = await createGame({ teamName: 'TestAPI'})
-      console.log(data)
-    })
+    // Reference Code
+    // onMounted(async () => {
+      // console.log('data')
+      // const data = await createGame({ teamName: 'TestAPI'})
+      // console.log(data)
+    // })
     return {
       gameId: computed(() => route.params.puzzleId)
       // puzzle: computed(() => puzzle)
