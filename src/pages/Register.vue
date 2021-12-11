2750021f5b445975f0407fffc78779e044fda88a
Andrew <>
2021-12-11 12:10:18 +0800

    Register front end pages

---
+++ src/pages/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="col text-center">
+    </div>
+    <div class="col text-center">
+        <q-input v-model="text" label="Team Name" />
+        <q-input v-model="text" label="Email (one email would do)" />
+    </div>
+    <div class="col text-center">
+    </div>
+   </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+//import { useRoute } from 'vue-router'
+// import { apiService } from 'src/apis/prismicApis'
+
+export default defineComponent({
+  name: 'RegisterPage',
+  components: {
+  },
+  setup() {
+    //const route = useRoute()
+    return {
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
