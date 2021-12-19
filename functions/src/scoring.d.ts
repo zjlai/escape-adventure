31079992389e004e271a2e829283bd894534adef
simplic8 <merv_lee@yahoo.com>
2021-12-19 20:04:03 +0800

    Added scoringGeo - geo distance scoring

--- functions/src/scoring.d.ts
+++ functions/src/scoring.d.ts
@@ -1,8 +1,19 @@
+import { firestore } from 'firebase-admin';
+
 export interface scoringInterface {
   gameId: string;
   puzzleRef: string;  
   hintsUsed: number;
   answer: string;
+  timeTaken: number;
+  hintsPenalty: number;  
+}
+
+export interface scoringGeoInterface {
+  gameId: string;
+  puzzleRef: string;  
+  hintsUsed: number;
+  answer: firestore.GeoPoint;
   timeTaken: number;
   hintsPenalty: number;  
 }
--- functions/src/scoring.ts
+++ functions/src/scoring.ts
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
 import { firestore } from "firebase-admin";
 import { fn, db } from "./index";
-import { scoringInterface } from "./scoring.d";
+import { scoringInterface, scoringGeoInterface } from "./scoring.d";
+const geofire = require("geofire-common");
 
 /* scoringText - Test scripts for firebase functions:shell
 CORRECT ANSWER (PERFECT SCORE)
@@ -278,3 +279,150 @@ export const scoringText = fn
       }
     });
 
+/* scoringGeo - Test scripts for firebase functions:shell
+CORRECT ANSWER (PERFECT SCORE)
+scoringGeo({gameId: 'Zmbfcdc5yzfoetZr85Ro', puzzleRef: 'geoPuzzle1', hintsPenalty: 0, answer: [1, 0], hintsUsed: 0, timeTaken: 40})
+
+CORRECT ANSWER (Distance shy of actual answer, but within maxDistKm)
+scoringGeo({gameId: 'Zmbfcdc5yzfoetZr85Ro', puzzleRef: 'geoPuzzle1', hintsPenalty: 0, answer: [1, 0.1], hintsUsed: 0, timeTaken: 40})
+
+WRONG ANSWER (Distance outside maxDistKm, resulting in zero score)
+scoringGeo({gameId: 'Zmbfcdc5yzfoetZr85Ro', puzzleRef: 'geoPuzzle1', hintsPenalty: 0, answer: [30, 30], hintsUsed: 0, timeTaken: 40})
+
+CORRECT ANSWER (FRACTION OF TIME)
+scoringGeo({gameId: 'Zmbfcdc5yzfoetZr85Ro', puzzleRef: 'geoPuzzle1', hintsPenalty: 0, answer: [1, 0], hintsUsed: 0, timeTaken: 500})
+
+CORRECT ANSWER (PENALIZE HINTS)
+scoringGeo({gameId: 'Zmbfcdc5yzfoetZr85Ro', puzzleRef: 'geoPuzzle1', hintsPenalty: 20, answer: [1, 0], hintsUsed: 1, timeTaken: 40})
+*/
+
+export const scoringGeo = fn
+    .https
+    .onCall(async (data: scoringGeoInterface) => {
+      // log incoming data
+      functions.logger.info(data);
+
+      // get game session data from firestore
+      const game = await db.collection(GAME_COLLECTION).doc(data.gameId).get();
+      if (!game.exists) {
+        console.log('Game Id not found');
+        return {
+          error: 'Game Id not found'
+        }
+      }
+      
+      // Get solutions/puzzle data      
+      var answer = new firestore.GeoPoint(0, 0);
+      var maxScore = 0;
+      //var minScore = 0;
+      var maxTime = 0;
+      var minTime = 0;
+      var next = "";
+      var timed = false;
+      var solutionType = "";
+      var maxDistKm = 0;
+      await db.collection(GAME_SOLUTIONS).where("puzzleRef", "==", data.puzzleRef).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          answer = doc.get('answer');
+          console.log("Coord is " + answer.longitude + ", " + answer.latitude);
+          maxScore = doc.get('maxScore');
+          maxDistKm = doc.get('maxDistKm');
+          //minScore = doc.get('minScore');
+          maxTime = doc.get('maxTime');
+          minTime = doc.get('minTime');
+          next = doc.get('next')
+          timed = doc.get('timed')
+          solutionType = doc.get('solutionType')
+        })
+      })
+    
+      // initialize Scoring
+      var score = 0;
+      maxScore = maxScore + 0;
+      maxTime = maxTime;
+      minTime = minTime;
+      next = next;
+      timed = timed;
+      solutionType = solutionType;
+      maxDistKm = maxDistKm;
+      
+      // check for geo distance between user input and solution answer (to move this into a separate module later, e.g., if boolean answer = true then proceed)            
+      var correctAnswer = true; // no answer is wrong, just different scores, therefore always true
+      const distInKm = geofire.distanceBetween(data.answer, [answer.latitude, answer.longitude]);
+
+      console.log ("Checking for the correct answer => " + data.answer + " is " + distInKm + "km away from " + answer.latitude + ", " + answer.longitude);
+
+      if(correctAnswer) {
+
+        //fracitionalize score by distance to geo answer, where maxDistKm is the maximum distance before zero score is awarded
+        if (distInKm > maxDistKm)
+          score = 0
+        else
+          score = (maxDistKm - distInKm) / maxDistKm * maxScore        
+
+        console.log("Score for distancing is " + score);
+
+        // fractionalize score by time taken, where
+        // if timeTaken <= minTime = 100%
+        // if timeTaken > maxTime = 0%
+        // else (maxTime - timeTaken) / (maxTime - minTime)
+        var timePercent = 0;
+        if(timed) {
+          if(data.timeTaken <= minTime) {
+            timePercent = 1
+          } else if (data.timeTaken >= maxTime) {
+            timePercent = 0
+          } else {
+            timePercent = (maxTime - data.timeTaken) / (maxTime - minTime)
+          }
+          
+          score = timePercent * score;
+        }
+
+        // retain rawScore before penalization
+        var rawScore = score;
+        
+        // subtract hints used from score
+        score = score - data.hintsPenalty;
+
+        // compute overall total score 
+        var overallTotalScore = game.get('totalScore') + score;
+
+        // update game record and add gameData/score record {gameData > answer, hintsPenalty, hintsUsed, puzzle, rawScore, solutionsType, time}                
+        await db.collection(GAME_COLLECTION).doc(data.gameId).update({
+          gameData: firestore.FieldValue.arrayUnion({
+            answer: data.answer,
+            hintsPenalty: data.hintsPenalty,
+            hintsUsed: data.hintsUsed,
+            puzzle: data.puzzleRef,
+            rawScore: rawScore,
+            solutionType: solutionType,
+            time: data.timeTaken,
+            totalScore: score
+          }),
+          stage: next,
+          totalScore: overallTotalScore
+        });
+
+        return {
+          gameId: game.id,
+          answer: data.answer,
+          puzzle: data.puzzleRef,
+          solutionType: solutionType,
+          hintsUsed: data.hintsUsed,
+          hintsPenalty: data.hintsPenalty,
+          next: next,
+          score: score,
+          time: data.timeTaken,
+          rawScore: rawScore,
+          puzzle_TotalScore: score,
+          overall_TotalScore: overallTotalScore
+        };
+
+      } else {
+        return {
+          error: 'Wrong answer'
+        }
+      }    
+
+    });
